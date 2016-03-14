11f29793cd4e966cefde1478d49a890798c1cd6b
taiweili <taiweili@umich.edu>
2016-03-14 11:59:30 -0400

    YAP

--- assignment.R
+++ assignment.R
@@ -1,4 +1,16 @@
+#require package: ggplot2, devtool, rio
+
+library(ggplot2)
+library("devtools")
+#install_github("leeper/rio")
+library("rio")
+
+#get the gapminder profile: import is a function in package of need devtool and rio 
+gapminder <- import("https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv")
 
 # correlation of population size, GDP and life expectency in different continent
-gap_graphic <- qplot(lifeExp, gdpPercap, data = gapminder, size = pop, color=continent) + scale_y_log10() + scale_size_area(max_size = 20)
+gap_graphic <- qplot(lifeExp, gdpPercap, data = gapminder, size = pop, color=continent) + scale_y_log10() + scale_size_area(max_size = 15)
+
+gap_graphic
+
 
