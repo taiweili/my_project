82c3df1b64b42003b20105c6a8577454cdb2c69c
taiweili <taiweili@umich.edu>
2016-03-14 11:59:30 -0400

    commit

--- assignment.R
+++ assignment.R
@@ -1,4 +1,5 @@
 
 # correlation of population size, GDP and life expectency in different continent
-gap_graphic <- qplot(lifeExp, gdpPercap, data = gapminder, size = pop, color=continent) + scale_y_log10() + scale_size_area(max_size = 20)
+gap_graphic <- qplot(lifeExp, gdpPercap, data = gapminder, size = pop, color=continent) + scale_y_log10() + scale_size_area(max_size = 15)
 
+run gap_graphic
